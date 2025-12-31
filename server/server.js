acd29d87c808011009d3608850367cb87594dd63
hirenahir02 <hirenahir509@gmail.com>
2026-01-01 00:04:14 +0530

    p

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { PeerServer } = require('peer');
 const app = express();
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
   res.send('FlashShare Signaling Server is Running... 🚀');

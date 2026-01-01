b2306282a42c1617f3265025ec48563491cf9c4f
hirenahir02 <hirenahir509@gmail.com>
2026-01-01 07:17:37 +0530

    port 8080 ok5

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const { ExpressPeerServer } = require('peer');
-
+const { PeerServer } = require('peer');
 const app = express();
-const PORT = process.env.PORT || 9000;
+const port = 9000;
 
-// test route
 app.get('/', (req, res) => {
-  res.send('🚀 FlashShare Server Running');
+  res.send('FlashShare Signaling Server is Running... 🚀');
 });
 
-// start express server
-const server = app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+const server = app.listen(port, () => {
+  console.log(`✨ FlashShare Server running on port ${port}`);
 });
 
-// ⚠️ DO NOT set port here
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  allow_discovery: true,
+const peerServer = PeerServer({
+  // port: 9000,
+  path: '/flashshare',
+  allow_discovery: true
 });
 
-// mount peer server
-app.use('/flashshare', peerServer);
-
-// logs
 peerServer.on('connection', (client) => {
-  console.log('Peer Connected:', client.getId());
+  console.log(`Client Connected: ${client.getId()}`);
 });
+
+peerServer.on('disconnect', (client) => {
+  console.log(`Client Disconnected: ${client.getId()}`);
+});
+
+console.log('📡 P2P Signaling active on port 9001');
+

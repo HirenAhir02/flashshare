95f0091d942289371f28c1587d381881d27db215
hirenahir02 <hirenahir509@gmail.com>
2026-01-01 07:34:55 +0530

    port 8080 ok10

--- server/server.js
+++ server/server.js
@@ -1,25 +1,12 @@
 const express = require('express');
-const { ExpressPeerServer } = require('peer');
-
 const app = express();
-const PORT = process.env.PORT;   // 🚨 VERY IMPORTANT
+
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('🚀 FlashShare Server Running');
+  res.send('🚀 FlashShare Backend Alive');
 });
 
-// START SERVER ON RAILWAY PORT
-const server = app.listen(PORT, () => {
-  console.log('Server running on', PORT);
-});
-
-// Attach peer server
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-});
-
-app.use('/flashshare', peerServer);
-
-peerServer.on('connection', (client) => {
-  console.log('Peer Connected:', client.getId());
+app.listen(PORT, () => {
+  console.log('Server started on port', PORT);
 });
